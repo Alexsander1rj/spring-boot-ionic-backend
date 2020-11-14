618894811a59c62268a292bd1813a77b3cf9f724
Alexsander <alexsander1rj@gmail.com>
2020-11-13 23:43:01 -0300

    Criação do Projetô

---
+++ cursomc/src/test/java/com/alexcaetano/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexcaetano.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
