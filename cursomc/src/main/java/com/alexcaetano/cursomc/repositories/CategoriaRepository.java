ac9f627af5af0bd19f518c2ea43b0983e7e47b3c
Alexsander <alexsander1rj@gmail.com>
2020-11-14 01:42:55 -0300

    Criando repository e service para Categoria

---
+++ cursomc/src/main/java/com/alexcaetano/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.alexcaetano.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alexcaetano.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
