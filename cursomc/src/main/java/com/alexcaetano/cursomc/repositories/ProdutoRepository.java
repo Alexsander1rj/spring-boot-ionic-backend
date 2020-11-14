b1e47b5a0f8486b9f9d8e84a67f5b496a849caf2
Alexsander <alexsander1rj@gmail.com>
2020-11-14 16:17:35 -0300

    Produto e associacao muitos para muitos

---
+++ cursomc/src/main/java/com/alexcaetano/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.alexcaetano.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alexcaetano.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
