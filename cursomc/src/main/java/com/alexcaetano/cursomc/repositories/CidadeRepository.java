7cb6abc63edb6f712d66f4e0996ed8310fd51576
Alexsander <alexsander1rj@gmail.com>
2020-11-15 11:16:58 -0300

    Estado e Cidade

---
+++ cursomc/src/main/java/com/alexcaetano/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.alexcaetano.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alexcaetano.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
